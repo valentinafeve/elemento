3b286129d410f2ec52580da64907862a1a4f1df4
gamendez98 <gamendez98@gmail.com>
2020-02-11 17:54:33 -0500

    ...

--- Structuring speech/nltk_standford.py
+++ Structuring speech/nltk_standford.py
@@ -1,11 +1,11 @@
 import os
 from nltk.parse import stanford
-os.environ['STANFORD_PARSER'] = '/path/to/standford/jars'
-os.environ['STANFORD_MODELS'] = '/path/to/standford/jars'
+os.environ['STANFORD_PARSER'] = '/home/gustavo/Downloads/stanford-parser-full-2018-10-17'
+os.environ['STANFORD_MODELS'] = '/home/gustavo/Downloads/stanford-parser-full-2018-10-17'
 
 parser = stanford.StanfordParser(model_path="/location/of/the/englishPCFG.ser.gz")
 sentences = parser.raw_parse_sents(("Hello, My name is Melroy.", "What is your name?"))
-print sentences
+print(sentences)
 
 # GUI
 for line in sentences:
