1dd4cb82634e1db7d8ec881b9a3987d554ca8dc6
valntinaf <feruerevalentina@gmail.com>
2020-02-18 13:26:06 -0500

    Generating SVG from dependency tree with Stanford

---
+++ Structuring speech/generating_trees/dparser.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from nltk.parse.corenlp import CoreNLPDependencyParser
+dep_parser = CoreNLPDependencyParser(url='http://localhost:9000')
+
+f = open("texts/text2", "r")
+sentence = f.read()
+parse, = dep_parser.raw_parse(sentence)
+
+f = open(str(datetime.now())+'.svg', 'w')
+svg = parse._repr_svg_()
+f.write(svg)
+
+print(parse.to_conll(4))
