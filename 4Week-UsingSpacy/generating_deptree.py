ec9f63bd28ebe8cdf05c000f4064545af6b5d53f
gamendez98 <gamendez98@gmail.com>
2020-03-09 13:19:30 -0500

    trees created. parsing by '.'

--- 4Week-UsingSpacy/generating_deptree.py
+++ 4Week-UsingSpacy/generating_deptree.py
@@ -3,20 +3,22 @@ from nltk.parse.corenlp import CoreNLPDependencyParser
 from nltk.parse.dependencygraph import DependencyGraph
 
 parser = CoreNLPDependencyParser(url='http://localhost:9000')
-for i in range(4,7):
+for i in range(1,7):
 
     filename = "text"+str(i)
     f = open("../Fragments_for_testing/"+filename, "r")
-    sentences = f.readlines()
+    sentences=[]
+    for L in f.readlines():
+        sentences += L.split('.')
     j=1
     for sentence in sentences:
-        parse, = parser.raw_parse(sentence)
-
-        conll = parse.to_conll(4)
-        dp = DependencyGraph(conll)
-        dotted = dp.to_dot()
-        graph = dp.nx_graph()
-        f = open(filename+'_'+str(j)+'.svg', 'w')
-        svg = dp._repr_svg_()
-        f.write(svg)
-        j+=1
+        if len(sentence)>1:
+            parse, = parser.raw_parse(sentence)
+            conll = parse.to_conll(4)
+            dp = DependencyGraph(conll)
+            dotted = dp.to_dot()
+            graph = dp.nx_graph()
+            f = open("outs/"+filename+'_'+str(j)+'.svg', 'w')
+            svg = dp._repr_svg_()
+            f.write(svg)
+            j+=1
